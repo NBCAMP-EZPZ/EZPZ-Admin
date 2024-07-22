1c1785ea76cf33ea7cd41d0b8bcefcc8a15a55c9
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-22 11:53:46 +0900

    init commit

---
+++ src/main/java/com/sparta/ezpzadmin/EzpzAdminApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ezpzadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzpzAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzpzAdminApplication.class, args);
+    }
+
+}
