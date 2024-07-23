6952c6040804e678965de2ad7edeb67faa3633c1
ydh5017 <ydh5017@naver.com>
2024-07-23 21:59:12 +0900

    feat [#1] : 팝업
    1. 상태별 팝업 목록 조회
    2. 팝업 상세 조회
    3. 팝업 승인/반려

--- src/main/java/com/sparta/ezpzadmin/domain/popup/service/ImageService.java
+++ src/main/java/com/sparta/ezpzadmin/domain/popup/service/ImageService.java
@@ -3,11 +3,9 @@ package com.sparta.ezpzadmin.domain.popup.service;
 import com.sparta.ezpzadmin.domain.popup.entity.Image;
 import com.sparta.ezpzadmin.domain.popup.entity.Popup;
 import com.sparta.ezpzadmin.domain.popup.repository.ImageRepository;
-import com.sparta.ezpzadmin.domain.popup.util.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -17,7 +15,6 @@ import java.util.List;
 public class ImageService {
 
     private final ImageRepository imageRepository;
-    private final S3Util s3Util;
 
     /**
      * 팝업으로 이미지 url 목록 조회
@@ -28,38 +25,5 @@ public class ImageService {
         return imageRepository.findAllByPopup(popup).stream()
                 .map(Image::getUrl)
                 .toList();
-    }
-
-    /**
-     * 팝업으로 이미지 목록 조회
-     * @param popup 팝업
-     * @return 이미지 목록
-     */
-    public List<Image> findAllImageByPopup(Popup popup) {
-        return imageRepository.findAllByPopup(popup);
-    }
-
-    /**
-     * 썸네일 삭제 (S3)
-     * @param thumbnailName 썸네일 저장명
-     */
-    public void deleteThumbnail(String thumbnailName) {
-        s3Util.deleteFile(thumbnailName);
-    }
-
-    /**
-     * 추가 사진 목록 삭제 (S3)
-     * @param images 추가 사진 목록
-     */
-    public void deleteImages(List<Image> images) {
-        if (CollectionUtils.isEmpty(images)) {
-            return;
-        }
-
-        for (Image image : images) {
-            s3Util.deleteFile(image.getName());
-        }
-
-        imageRepository.deleteAll(images);
     }
 }
