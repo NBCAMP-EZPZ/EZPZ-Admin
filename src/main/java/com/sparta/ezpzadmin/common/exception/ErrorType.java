c0832d8c947733ddec1f9c419727b29330c84a70
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-24 12:20:39 +0900

    Feat [#5] : 쿠폰 관련 ErrorType 추가

--- src/main/java/com/sparta/ezpzadmin/common/exception/ErrorType.java
+++ src/main/java/com/sparta/ezpzadmin/common/exception/ErrorType.java
@@ -4,16 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @Getter
 @AllArgsConstructor
 public enum ErrorType {
 
     // JWT
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다. 다시 로그인 해주세요."),
-    CARD_ACCESS_FORBIDDEN(HttpStatus.FORBIDDEN, "카드 작성자 및 매니저만 접근할 수 있습니다."),
+    INVALID_REFRESH_TOKEN(UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다. 다시 로그인 해주세요."),
+    CARD_ACCESS_FORBIDDEN(FORBIDDEN, "카드 작성자 및 매니저만 접근할 수 있습니다."),
 
     // Admin
-    DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 관리자 아이디입니다."),
+    DUPLICATED_USERNAME(BAD_REQUEST, "이미 존재하는 관리자 아이디입니다."),
 
     // Popup
 
@@ -23,7 +26,9 @@ public enum ErrorType {
 
     // Reservation
 
-    //
+    // Coupon
+    DUPLICATED_COUPON_NAME(BAD_REQUEST, "이미 존재하는 쿠폰명입니다."),
+    INVALID_COUPON_EXPIRATION(BAD_REQUEST, "만료일이 현재 날짜보다 과거입니다.")
 
     ;
     private final HttpStatus httpStatus;
